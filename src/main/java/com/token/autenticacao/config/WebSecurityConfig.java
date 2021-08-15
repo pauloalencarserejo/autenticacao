e02b1ca1e8deb087d4b4a5a59de8be8e327c6e19
Paulo Alencar <34141077+pauloalencarserejo@users.noreply.github.com>
2021-08-15 16:52:57 -0300

    feat: adicionando configuracoes do websecurityadapter

--- src/main/java/com/token/autenticacao/config/WebSecurityConfig.java
+++ src/main/java/com/token/autenticacao/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.token.autenticacao.config;
 
+import javax.servlet.http.HttpServletResponse;
+
+import com.token.autenticacao.service.UserService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,19 +19,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserService userService;
+    private final JwtConfiguration jwtConfiguration;
+
+    private final TokenService tokenService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated();
+        http.csrf().disable().cors().configurationSource(req -> new CorsConfiguration().applyPermitDefaultValues())
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling()
+                .authenticationEntryPoint((req, res, e) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+                .addFilter(new AuthFilter(authenticationManager(), jwtConfiguration, tokenService)).authorizeRequests()
+                .antMatchers(jwtConfiguration.getUrlLogin()).permitAll().anyRequest().authenticated();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // TODO Auto-generated method stub
-        super.configure(auth);
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
