bbd459654cec1ca8e297024dd6cb48a33d2d8b68
Paulo Alencar <34141077+pauloalencarserejo@users.noreply.github.com>
2021-08-15 16:53:35 -0300

    feat: adicionando filtro de autenticacao do usuario

--- src/main/java/com/token/autenticacao/config/AuthFilter.java
+++ src/main/java/com/token/autenticacao/config/AuthFilter.java
@@ -1,5 +1,70 @@
 package com.token.autenticacao.config;
 
-public class AuthFilter {
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.token.autenticacao.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authManager;
+    private final JwtConfiguration jwtConfiguration;
+    private final TokenService tokenService;
+
+    @Override
+    @SneakyThrows
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        log.info("Attemping authentication");
+
+        User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Nao foi possivel logar tente novamente");
+        }
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                user.getUsername(), user.getPassword());
+
+        usernamePasswordAuthenticationToken.setDetails(user);
+
+        return authManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        String token = tokenService.createToken(authResult);
+
+        // adicionando token ao header
+        response.addHeader("Access-Control-Expose-Headers", "XSRF-TOKEN, " + jwtConfiguration.getHeader());
+        response.addHeader(jwtConfiguration.getHeader(), jwtConfiguration.getPrefix() + token);
+
+        // retornando o token pelo conteudo da resposta
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        response.getWriter().write(
+                "{\"" + jwtConfiguration.getHeader() + "\":\"" + jwtConfiguration.getPrefix() + " " + token + "\"}");
+    }
 }
